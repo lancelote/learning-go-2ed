e7d0db1cf9c8c48898e4f05cebe7b8c8af869060
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-01-18 18:38:54 +0100

    chapter 5 exercise 3

---
+++ ch05/exr3.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func prefixer(prefix string) func(string) string {
+	return func(line string) string {
+		return prefix + " " + line
+	}
+}
+
+func main() {
+	helloPrefix := prefixer("Hello")
+	
+	fmt.Println(helloPrefix("Bob"))
+	fmt.Println(helloPrefix("Alice"))
+}
