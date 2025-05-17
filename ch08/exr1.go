c7c0f882bc207b22f565843745948a3f4dca05e4
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-05-17 19:08:51 +0200

    fix formatting

--- ch08/exr1.go
+++ ch08/exr1.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Number interface {
-        ~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64
 }
 
 func Double[T Number](t T) T {
-        return t * 2
+	return t * 2
 }
 
 func main() {
-        fmt.Println(Double(42))
-        fmt.Println(Double(42.0))
+	fmt.Println(Double(42))
+	fmt.Println(Double(42.0))
 }
