f350296b9b55201f5d271024ea4b212de189dd01
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-05-17 18:55:43 +0200

    chapter 8 exercise 1

---
+++ ch08/exr1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Number interface {
+        ~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64
+}
+
+func Double[T Number](t T) T {
+        return t * 2
+}
+
+func main() {
+        fmt.Println(Double(42))
+        fmt.Println(Double(42.0))
+}
