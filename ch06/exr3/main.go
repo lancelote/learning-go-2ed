41d1734dc4408ee9ba0e1c9307eeacbd36ad1810
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-05-27 18:28:51 +0200

    fix `staticcheck` warning

--- ch06/exr3/main.go
+++ ch06/exr3/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Person struct {
 	FirstName string
 	LastName  string
@@ -18,5 +22,9 @@ func main() {
 	people := make([]Person, 0, 10_000_000)
 	for i := 0; i < 10_000_000; i++ {
 		people = append(people, MakePerson("John", "Doe", 42))
+	}
+
+	for _, p := range people {
+		fmt.Println(p.FirstName)
 	}
 }
